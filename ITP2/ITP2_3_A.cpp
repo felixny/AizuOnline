2717c77bb3e94e6ce7620e4b9f9b962bb91be12b
Felix <nampanya.f@northeastern.edu>
2021-10-29 14:15:33 -0400

    ITP2_3_A added

---
+++ ITP2/ITP2_3_A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cin >> a >> b >>c;
+
+    cout << min(a,min(b,c)) << " " << max(a,max(b,c)) << endl;
+
+}
