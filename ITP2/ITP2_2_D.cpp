b568df3b6dafc562350eb3fbd8e93bd24aeeac6e
Felix <nampanya.f@northeastern.edu>
2021-10-29 11:30:15 -0400

    added

--- ITP2/ITP2_2_D.cpp
+++ ITP2/ITP2_2_D.cpp
@@ -1,1 +1,45 @@
 // ITP2_2_D
+#include <deque>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int q;
+  int command;
+  int x;
+  int n;
+  int t;
+  int S; 
+  cin >> n >> q;
+  vector<list<int>> s(n);  // 2d array?
+
+
+  for (int i = 0; i < q; i++) {
+    cin >> command;
+    if (command == 0) {
+      cin >> t >> x;
+      s[t].push_back(x);
+      // s[t].insert(s[t].end(),x);
+
+    } else if (command == 1) {
+      cin >> t;
+      if (!s[t].empty()) {
+        auto itr = s[t].begin();
+        cout << *itr;
+        while (++itr != s[t].end()) cout << " " << *itr;
+      }
+      cout << endl;
+    } else {
+      cin >> S >> t;
+      s[t].splice(s[t].end(), s[S]);
+      s[S].clear();
+    }
+  }
+
+  return 0;
+}
