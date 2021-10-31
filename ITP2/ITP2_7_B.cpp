acf8d22c8ecaa3dbfa8b01b39aca6c227614cd13
Felix <nampanya.f@northeastern.edu>
2021-10-31 14:19:04 -0400

    ITP2_7_B added

---
+++ ITP2/ITP2_7_B.cpp
@@ -0,0 +1,33 @@
+// ITP2_7_B
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <tuple>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+  int q;
+  cin >> q;
+  set<int> s;
+
+  for (int i = 0; i < q; i++) {
+    int command, num;
+    cin >> command;
+    if (command == 0) {
+      cin >> num;
+      s.insert(num);
+      cout << s.size() << endl;
+
+    } else if (command == 1) {
+      cin >> num;
+      cout << s.count(num) << endl;
+    } else if (command == 2){
+        cin >> num;
+        s.erase(num);
+    }
+  }
+  return 0;
+}
