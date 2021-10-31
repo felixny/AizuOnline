132cd12affc5d935e12adb1a4aa4e8ce9e74ceef
Felix <nampanya.f@northeastern.edu>
2021-10-31 13:23:44 -0400

    ITP2_6_B added

--- ITP2/ITP2_6_B.cpp
+++ ITP2/ITP2_6_B.cpp
@@ -23,15 +23,15 @@ int main() {
   for (int i = 0; i < m; i++) {
     cin >> b[i];
   }
- /*  int f = 0;
-  for (int i = 0; i < m; i++) {
-    while (f < a.size() && a[f] != b[i]) f++;
-    if (f == a.size()) {
-      cout << 0 << endl;
-      return 0;
-    }
-  }
-  cout << 1 << endl; */
+  /*  int f = 0;
+   for (int i = 0; i < m; i++) {
+     while (f < a.size() && a[f] != b[i]) f++;
+     if (f == a.size()) {
+       cout << 0 << endl;
+       return 0;
+     }
+   }
+   cout << 1 << endl; */
 
   cout << includes(a.begin(), a.end(), b.begin(), b.end()) << endl;
 
