5cb96c6cca700d96ee7b427859f22041319974b2
Felix <nampanya.f@northeastern.edu>
2021-10-31 13:14:01 -0400

    ITP2_6_B added

---
+++ ITP2/ITP2_6_B.cpp
@@ -0,0 +1,39 @@
+// ITP2_6_B
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <tuple>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  int m;
+  cin >> m;
+  vector<int> b(m);
+
+  for (int i = 0; i < m; i++) {
+    cin >> b[i];
+  }
+ /*  int f = 0;
+  for (int i = 0; i < m; i++) {
+    while (f < a.size() && a[f] != b[i]) f++;
+    if (f == a.size()) {
+      cout << 0 << endl;
+      return 0;
+    }
+  }
+  cout << 1 << endl; */
+
+  cout << includes(a.begin(), a.end(), b.begin(), b.end()) << endl;
+
+  return 0;
+}
