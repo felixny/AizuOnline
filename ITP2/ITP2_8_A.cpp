c68fc4c49b5eee4b0e0c3ed84842a838cda83e9c
Felix <nampanya.f@northeastern.edu>
2021-10-31 16:15:51 -0400

    ITP2_8_A added

--- ITP2/ITP2_8_A.cpp
+++ ITP2/ITP2_8_A.cpp
@@ -1,4 +1,4 @@
-// ITP2_8_A
+// ≈
 #include <algorithm>
 #include <iostream>
 #include <map>
@@ -11,19 +11,18 @@ using namespace std;
 
 int main() {
   int q;
-  string key; int x;
+  string key;
+  int x;
   cin >> q;
   map<string, int> m;
   for (int i = 0; i < q; i++) {
     int command;
     cin >> command;
     if (command == 0) {
-     
       cin >> key >> x;
       m[key] = x;
     } else if (command == 1) {
       cin >> key;
-
       cout << m[key] << endl;
     }
   }
