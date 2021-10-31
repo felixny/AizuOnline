4f9bea621a9a0a30b666e83a776a04dec435aeee
Felix <nampanya.f@northeastern.edu>
2021-10-31 16:13:10 -0400

    ITP2_8_A added

---
+++ ITP2/ITP2_8_A.cpp
@@ -0,0 +1,32 @@
+// ITP2_8_A
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <tuple>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+  int q;
+  string key; int x;
+  cin >> q;
+  map<string, int> m;
+  for (int i = 0; i < q; i++) {
+    int command;
+    cin >> command;
+    if (command == 0) {
+     
+      cin >> key >> x;
+      m[key] = x;
+    } else if (command == 1) {
+      cin >> key;
+
+      cout << m[key] << endl;
+    }
+  }
+
+  return 0;
+}
